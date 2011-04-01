2945419971ccdc99a0e4ac9bfa50be8fddaca4d4
arrimurri <patrik.marjanen@helsinki.fi>
2011-04-01 17:20:48 +0300

    The Class that is in charge of importing the courses from the json interface. The importing is done, but I still have to get it to create the courses and then list the courses, so that they can be chosen (so that the user can register to courses).

--- CourseImporter.java
+++ CourseImporter.java
@@ -1,14 +1,58 @@
 package Ohtu;
 
+import java.util.ArrayList;
+import java.net.*;
+import java.io.*;
+
+import com.google.gson.Gson;
+
 public class CourseImporter
 {
-	public CourseImporter()
+    String url;
+    Gson g;
+
+	public CourseImporter(String url)
 	{
-		
+        this.url = url;
+        g = new Gson();
 	}
 	
-	public void importCourses(String url)
+	public ArrayList<Course> importCourses()
 	{
-		System.out.println("Todo, Imported courses from: "+url);
+        ArrayList<Course> retList = new ArrayList<Course>();
+
+        String jsonString = "";
+        try
+        {
+            jsonString = readJSONFromUrl();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        ImportCourses ic = new Gson().fromJson(jsonString, ImportCourses.class);
+
+        return retList;
 	}
+
+    private String readJSONFromUrl() throws Exception
+    {
+        String retString = "";
+        URL url = new URL(this.url);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                    url.openStream()));
+
+        String temp;
+        while ((temp = in.readLine()) != null)
+        {
+            retString += temp;
+        }
+
+        in.close();
+
+        return retString;
+    }
 }
